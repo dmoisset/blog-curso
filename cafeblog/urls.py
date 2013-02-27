765bf588bde8fdc54d0e95418b6c9196e34ae68b
pablotorres <pablotorrescba@gmail.com>
2013-02-27 15:00:42 -0300

    Se modifica hubicacion de carpetas de los templates de regitracion, y se modifica la configuracion de las vistas.

--- cafeblog/urls.py
+++ cafeblog/urls.py
@@ -10,24 +10,19 @@ urlpatterns = patterns('',
 
     url(r'^user/password_reset/$', 
             'django.contrib.auth.views.password_reset', 
-            {'post_reset_redirect' : '/user/password_reset/done/',
-            'template_name':'cafeblog/registration/password_reset_form.html',
-            'email_template_name' : 'cafeblog/registration/password_reset_email.html'},
-            name="password_reset"
+            {'post_reset_redirect' : '/user/password_reset/done/'},
+            name="password_reset",
         ),
     url(r'^user/password_reset/done/$',
             'django.contrib.auth.views.password_reset_done',
-            {'template_name':'cafeblog/registration/password_reset_done.html'}
         ),
 
     url(r'^user/password_reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', 
             'django.contrib.auth.views.password_reset_confirm', 
-            {'post_reset_redirect' : '/user/password_reset/complete/',
-            'template_name' : 'cafeblog/registration/password_reset_confirm.html'},
-            name='password_reset_confirm'
+            {'post_reset_redirect' : '/user/password_reset/complete/'},
+            name='password_reset_confirm',
         ),
 
     url(r'^user/password_reset/complete/$', 
-        'django.contrib.auth.views.password_reset_complete',
-        {'template_name' : 'cafeblog/registration/password_reset_complete.html'},)
+        'django.contrib.auth.views.password_reset_complete',),
 )
