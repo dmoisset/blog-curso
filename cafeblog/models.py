21c3deb056b54fde7a7ef1d161cc6cfd019fb9b9
Carlos Matías de la Torre <cmdelatorre@gmail.com>
2013-02-27 18:08:22 -0300

    Created Blog model and initial migration

--- cafeblog/models.py
+++ cafeblog/models.py
@@ -1,5 +1,21 @@
 import datetime
 from django.utils import timezone
 from django.db import models
-from django.db.models import Sum, Avg
 from django.contrib.auth.models import User
+
+class Blog(models.Model):
+    """A Blog to post stuff"""
+    title = models.CharField(max_length=250, unique=True)
+    description = models.TextField(blank=True)
+    administrator = models.ForeignKey(User, related_name='admin') #6
+    authors = models.ManyToManyField(User) #6
+    #tags = #8
+
+    def __unicode__(self):
+        return self.title
+
+
+class Post(models.Model):
+    """A Post in some blog"""
+    blog = models.ForeignKey(Blog) #13
+    #20: titulo, contenido, autor, fecha de publicacion, fecha del ultimo cambio y etiquetas
