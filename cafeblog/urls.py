0ffaea98d1f07563b04ac70bc7c477fca4b862a3
Felipe Esposito <feliesp@gmail.com>
2013-02-27 11:47:09 -0300

    removed comment at fart

--- cafeblog/urls.py
+++ cafeblog/urls.py
@@ -4,7 +4,6 @@ from cafeblog import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
-    #Logueo y Deslogueo
     url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'cafeblog/login.html'}, name='login'),
     url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}, name='logout'),
 )
