ae2217956af9411aa0a5cf20e380222edbbc82e6
Felipe Esposito <feliesp@gmail.com>
2013-03-01 11:20:00 -0300

    added media serving capabilities

--- cafeblog_com/urls.py
+++ cafeblog_com/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
 
 admin.autodiscover()
 
@@ -11,3 +12,7 @@ urlpatterns = patterns('',
 )
 
 urlpatterns += staticfiles_urlpatterns()
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),)
