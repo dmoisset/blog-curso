1c558043af6219a3874fe72c45e773b4974fa619
Hernan Lozano <hernantz@gmail.com>
2013-03-01 18:08:10 -0300

    Added tests for new article view.

--- cafeblog/tests/tests.py
+++ cafeblog/tests/tests.py
@@ -2,10 +2,20 @@ from django.test import TestCase
 from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
 from model_factories import UserFactory, BlogFactory
+from django.core.exceptions import ObjectDoesNotExist
+from django.test.client import RequestFactory
+from cafeblog.views import edit_article, NO_UNIQUE_TITLE_ERROR
+from cafeblog.models import Article
+from django.utils import timezone
+from mock import patch
+import datetime
 
 
 def clear_db():
     User.objects.all().delete()
+
+
+TEST_PASSWORD = 'testpass'
 
 
 class CafeBlogViewsTest(TestCase):
@@ -110,9 +120,9 @@ class NewBlogViewTest(TestCase):
         """
         Create a logged in user
         """
-        self.logged_user = UserFactory(password='testpass')
+        self.logged_user = UserFactory(password=TEST_PASSWORD)
         self.client.login(username=self.logged_user.username,
-                          password='testpass')
+                          password=TEST_PASSWORD)
 
     def test_get_new_blog_view(self):
         """
@@ -161,3 +171,158 @@ class NewBlogViewTest(TestCase):
 
     def tearDown(self):
         clear_db()
+
+
+class NewArticleViewTest(TestCase):
+    def setUp(self):
+        """
+        Create a logged in user, an author and a blog
+        """
+        self.logged_user = UserFactory(password=TEST_PASSWORD)
+        self.client.login(username=self.logged_user.username,
+                          password=TEST_PASSWORD)
+        self.author = UserFactory(password=TEST_PASSWORD)
+        self.blog = BlogFactory(administrator=self.logged_user)
+        self.blog.authors.add(self.author)
+
+        self.stranger = UserFactory(password=TEST_PASSWORD)
+
+    def tearDown(self):
+        clear_db()
+
+    def _blog_url(self):
+        """
+        Returns the blog's url where the new article is going to be created
+        """
+        return reverse('cafeblog:new_article',
+                       kwargs={'blog_pk': self.blog.pk})
+
+    def _art_data(self):
+        """
+        Returns some post data to create an article against this view
+        """
+        return {'title': 'Test article', 'contents': 'Test content'}
+
+    def test_get_new_article_view(self):
+        """
+        Expect 200 status code for a logged in user
+        """
+        response = self.client.get(self._blog_url())
+        self.assertEquals(response.status_code, 200)
+
+    def test_new_article_template_used(self):
+        url = self._blog_url()
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, 'cafeblog/article_form.html')
+
+    def test_get_new_article_view_login_required(self):
+        """
+        Login is required when creating a new article
+        """
+        self.client.logout()  # Ensure we are logged out
+        url = self._blog_url()
+        redirect_url = '{0}?next={1}'.format(reverse('cafeblog:login'), url)
+        response = self.client.get(url)
+        self.assertRedirects(response, redirect_url)
+
+    def test_post_new_article_view_login_required(self):
+        """
+        Login is required when creating a new article
+        """
+        self.client.logout()  # Ensure we are logged out
+        url = self._blog_url()
+        redirect_url = '{0}?next={1}'.format(reverse('cafeblog:login'), url)
+        response = self.client.post(url)
+        self.assertRedirects(response, redirect_url)
+
+    def test_blog_exists(self):
+        pass
+
+    def test_admin_can_create_article(self):
+        """
+        The admin of a blog can create articles for it.
+        """
+        url = self._blog_url()
+        self.client.post(url, self._art_data())
+        article = self.logged_user.article_set.get(title='Test article',
+                                                   contents='Test content')
+        self.assertEquals(article.author.pk, self.logged_user.pk)
+
+    def test_author_can_create_article(self):
+        """
+        The author of a blog can create articles for it.
+        """
+        # Login the author
+        self.client.login(username=self.author.username, password=TEST_PASSWORD)
+        url = self._blog_url()
+        self.client.post(url, self._art_data())
+        article = self.author.article_set.get(title='Test article',
+                                                   contents='Test content')
+        self.assertEquals(article.author.pk, self.author.pk)
+
+    def test_create_new_article_with_unauthorized_user(self):
+        """
+        An unauthorized_user shoud get a 403 status code
+        """
+        # Login an stranger
+        self.client.login(username=self.stranger.username, password=TEST_PASSWORD)
+
+        url = self._blog_url()
+        resp = self.client.post(url, self._art_data())
+        self.assertEquals(resp.status_code, 403)
+
+    def test_non_author_cannot_create_article(self):
+        """
+        Only blog authors create articles for it.
+        """
+        # Login an stranger
+        self.client.login(username=self.stranger.username, password=TEST_PASSWORD)
+
+        url = self._blog_url()
+        self.client.post(url, self._art_data())
+
+        with self.assertRaises(ObjectDoesNotExist):
+           self.stranger.article_set.get(title='Test article')
+
+    def test_article_belongs_to_blog(self):
+        """
+        An article should be created on the given blog only
+        """
+        url = self._blog_url()
+        self.client.post(url, self._art_data())
+
+        art = self.blog.article_set.get(title='Test article')
+        self.assertEquals(art.blog.pk, self.blog.pk)
+
+    def test_article_title_is_unique(self):
+        """
+        An article should have a unique title within a blog
+        """
+        art_data = self._art_data()
+        art_data.update({
+            'author': self.logged_user,
+            'pub_date': timezone.now(),
+            'creation_time': timezone.now(),
+            'last_modified': timezone.now()
+        })
+        art = Article(**art_data)
+        self.blog.article_set.add(art)
+
+        # Now try creating an article with the same title again
+        url = self._blog_url()
+        resp = self.client.post(url, self._art_data())
+        self.assertFormError(resp, 'article_form', 'title', NO_UNIQUE_TITLE_ERROR)
+
+    def test_article_publication_date(self):
+        """
+        A newly created article shoud have the creation time set to now
+        """
+        now = datetime.datetime(2013, 12, 1, 12, 30, 50, 0, tzinfo=timezone.get_default_timezone())
+
+        url = self._blog_url()
+        with patch.object(timezone, 'now', return_value=now):
+            req = RequestFactory().post(url, self._art_data())
+            req.user = self.logged_user
+            edit_article(req, self.blog.pk)
+        art = self.logged_user.article_set.get(title='Test article')
+        self.assertEquals(art.creation_time, now)
