bad60f2fbbb58d868c2637c824812c3565fa2ac8
Felipe Esposito <feliesp@gmail.com>
2013-03-01 11:11:18 -0300

    added profile and profile_edit views to urls

--- cafeblog/urls.py
+++ cafeblog/urls.py
@@ -4,6 +4,8 @@ from cafeblog import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
+    url(r'^profile/$', views.profile, name='profile'),
+    url(r'^profile_edit/$', views.profile_edit, name='profile_edit'),
     url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'cafeblog/login.html'}, name='login'),
-    url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}, name='logout'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/cafeblog/'}, name='logout'),
 )
